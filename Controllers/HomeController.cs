99c5ce106ff36088371941b605d02b7bc937d809
KMX\KmX <guydo.k@yandex.by>
2021-11-18 23:18:41 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coursework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
