ad3b9e9c84fe0cd2304e3041cf442c1a416303f7
KmX <guydo.K@yandex.by>
2021-11-26 00:22:02 +0300

    Изменен класс UserRating, изменено представление

--- Domain/Entities/ApplicationUser.cs
+++ Domain/Entities/ApplicationUser.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using Coursework.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace Coursework.Domain.Entities
 {
-    public class ApplicationUser: IdentityUser<Guid>
+    public class ApplicationUser : IdentityUser<Guid>
     {
         public string AvatarUrl { get; set; }
+        [InverseProperty("User")]
         public ICollection<UserRating> Ratings { get; set; }
     }
 }
--- Models/UserRating.cs
+++ Models/UserRating.cs
@@ -6,6 +6,7 @@ namespace Coursework.Models
     {
         public int Id { get; set; }
         public ApplicationUser User { get; set; }
+        public ApplicationUser UserRater { get; set; }
         public int Rating { get; set; }
     }
 }
