751e45f3730cd0dfe749d911de660b0c8b1af5a9
KmX <guydo.K@yandex.by>
2021-11-26 00:22:02 +0300

    Изменен класс UserRating, изменено представление

--- Migrations/20211125211938_EditedUserRating.Designer.cs
+++ Migrations/20211125211938_EditedUserRating.Designer.cs
@@ -4,14 +4,16 @@ using Coursework.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Coursework.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211125211938_EditedUserRating")]
+    partial class EditedUserRating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
