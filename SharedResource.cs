2c530b0190e8839261b3f812ec18dc398da2927a
KMX\KmX <guydo.k@yandex.by>
2021-11-27 22:14:03 +0300

    Добавление возможности менять язык приложения

---
+++ SharedResource.cs
@@ -0,0 +1,7 @@
+﻿namespace Coursework
+{
+    public class SharedResource
+    {
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Coursework.Domain;
 using Coursework.Domain.Entities;
 using Coursework.Services;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +25,8 @@ namespace Coursework
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 {
                     options.UseNpgsql(Configuration.GetConnectionString("PostgreSql"));
@@ -46,9 +50,23 @@ namespace Coursework
                 options.AccessDeniedPath = new PathString("/Authorization/AccessDenied");
             });
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddViewLocalization();
 
             services.AddSingleton<MarkdownToHtmlService>();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("ru"),
+                    new CultureInfo("en")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture("ru");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -62,6 +80,7 @@ namespace Coursework
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+            app.UseRequestLocalization();
             app.UseStaticFiles();
 
             app.UseRouting();
